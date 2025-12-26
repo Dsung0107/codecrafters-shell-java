0a0da25a08abdab2df5729692ff76513494526fc
Derek Sung <d.sung@ufl.edu>
2025-12-26 13:35:33 -0500

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,12 +10,24 @@ public class Main {
             System.out.print("$ ");
             String input = in.nextLine();
 
-            if ((input.length() > 5) && (input.substring(0, 5).equals("echo "))) {
+            if ((input.length() > 4) && (input.substring(0, 5).equals("echo "))) {
                 System.out.println(input.substring(5));
             }
             else if (input.equals("exit")) {
                 break;
             }
+            else if ((input.length() > 4) && (input.substring(0,5).equals("type "))) {
+                String after = input.substring(5);
+                if ((after.equals("echo")) || (after.equals("exit")) || (after.equals("type"))) {
+                    System.out.println(after + " is a shell builtin");
+                }
+                else {
+                    System.out.println(after + ": not found");
+                }
+            }
+
+
+
             else {
                 System.out.println(input + ": command not found");
             }
