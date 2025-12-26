f592970852caf82b28d820436e494e4bbb656993
Derek Sung <d.sung@ufl.edu>
2025-12-26 09:40:43 -0500

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         System.out.print("$ ");
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        if (input != "") {
+            System.out.print(input + ": command not found");
+        }
     }
 }
