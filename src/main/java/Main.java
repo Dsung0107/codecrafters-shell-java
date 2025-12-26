567eba4d2892c3f1f6791350b49070399d376664
Derek Sung <d.sung@ufl.edu>
2025-12-26 09:49:13 -0500

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
-        System.out.print("$ ");
+
         Scanner in = new Scanner(System.in);
-        String input = in.nextLine();
-        if (input != "") {
-            System.out.print(input + ": command not found");
+        while (true) {
+
+            System.out.print("$ ");
+            String input = in.next();
+            if (input != "") {
+                System.out.println(input + ": command not found");
+            }
+
+
         }
     }
 }
