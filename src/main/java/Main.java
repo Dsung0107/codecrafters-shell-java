49fe4ff767439fdc0351c4b73bccc8d4f6bdbd8c
Derek Sung <d.sung@ufl.edu>
2026-07-04 21:52:00 -0400

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,7 @@ public class Main {
     public static String redirectTarget = null;
 
 
-    
+
     public static void main(String[] args) throws Exception {
         availableCommands.addAll(List.of(new String[]{"echo", "type", "exit", "pwd", "cd"}));
         Scanner in = new Scanner(System.in);
@@ -77,41 +77,61 @@ public class Main {
             redirectTarget = null;
         }
     }
+    
 
 
-
-    public static void executeCMD(String[] command, String input) throws IOException, InterruptedException {
+    public static int executeCMD(String[] command, String input) throws IOException, InterruptedException {
         if ((command[0].equals("echo")) && (command.length > 1)) {
             for (int i = 1; i < command.length; i++) {
                 System.out.print(command[i] + " ");
             }
             System.out.printf("\n");
+            return 0;
         }
         else if ((command[0].equals("type")) && (command.length > 1)) {
-            getType(command);
+            returngetType(command);
+            
         }
         else if (command[0].equals("pwd")) {
             System.out.println(dirOG);
+            return 0;
         }
         else if ((command[0].equals("cd")) && (command.length > 1)) {
-            changeDirectory(command);
+            return changeDirectory(command);
         }
         else if (findPATH(command) == true) {
             executeCommand(command);
+            return 0;
         }
         else {
             System.out.println(input + ": command not found");
+            return -1;
+        }
+        return 0;
+    }
+
+    public static boolean getError(String[] command, String input) throws IOException, InterruptedException {
+        if (executeCMD(command, input) == -1) {
+            return true;
+        }
+        else {
+            return false;
         }
     }
 
-
-
-    public static String[] redirectOutput(String[] arguments) throws FileNotFoundException {
+    public static String[] redirectOutput(String[] arguments, String input) throws FileNotFoundException {
         if (arguments.length > 2) {
             if (arguments[arguments.length-2].equals(">") ||
                     arguments[arguments.length-2].equals("1>")) {
                 redirectTarget = arguments[arguments.length-1];
                 arguments = Arrays.copyOfRange(arguments, 0, arguments.length - 2);
+            }
+            else if (arguments[arguments.length-2].equals("2>")) {
+                if (getError(arguments, input)) {
+                    redirectError = arguments[arguments.length-1];
+                    arguments = Arrays.copyOfRange(arguments, 0, arguments.length - 2);
+                }
+                
             }
         }
         return arguments;
@@ -134,7 +154,7 @@ public class Main {
                 }
             }
             System.out.println(commands[1] + ": not found");
-            return 0;
+            return -1;
         }
         return 0;
     }
@@ -183,7 +203,7 @@ public class Main {
 
 
 
-    public static void changeDirectory(String[] command) {
+    public static int changeDirectory(String[] command) {
         Path target = dirOG.resolve(command[1]).toAbsolutePath().normalize();
         if (command[1].equals("~")) {
             command[1] = System.getenv("HOME");
@@ -194,7 +214,9 @@ public class Main {
         }
         else {
             System.out.println("cd: " + command[1] + ": No such file or directory");
+            return -1;
         }
+        return 0;
     }
 
 
