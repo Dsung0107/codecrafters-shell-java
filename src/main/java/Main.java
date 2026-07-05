c5bd6a049a682c7e5ba450a3fc3b0766707ed9de
Derek Sung <d.sung@ufl.edu>
2026-07-05 12:10:41 -0400

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,7 +23,9 @@ public class Main {
             System.out.print("$ ");
             String input = in.nextLine();
             if (input.equals("exit")) {
+                in.close();
                 break;
+                
             }
             ArrayList<String> response = new ArrayList<>();
             StringBuilder echoReturn = new StringBuilder();
